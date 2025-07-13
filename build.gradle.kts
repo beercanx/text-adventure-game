e9cd1b2278fb1c5b24a0c8bbe9f26a708f5018ec
James Bacon <james@baconi.co.uk>
2025-07-13 09:43:31 +0100

    Upgraded Gradle wrapper to 8.14.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,1 @@
-// Here so what when './gradlew wrapper --gradle-version $LATEST_VERSION' is run it always uses the all distro.
-tasks.withType<Wrapper>().configureEach {
-    distributionType = Wrapper.DistributionType.ALL
-}
+
