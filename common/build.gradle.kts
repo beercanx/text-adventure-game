36c6bdb90e19a590c0a21cff9044cefc14e1c12e
James Bacon <james@baconi.co.uk>
2025-11-21 13:55:00 +0000

    Migrated dependencies to be managed via a version catalog

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,3 +1,28 @@
 plugins {
-    id("text-adventure-game.common")
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.plugin.serialization)
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+dependencies {
+
+    api(kotlin("reflect"))
+
+    // Logging
+    api(libs.logback.classic)
+
+    // Configuration
+    api(libs.typesafe.config)
+    api(libs.kotlinx.serialization.hocon)
+
+    // Coroutines - Aligns to the version brought in by "dev.kord:kord-core:0.8.0-M17"
+    api(libs.kotlinx.coroutines.core)
 }
